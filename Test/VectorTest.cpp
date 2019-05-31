33b20c881592fdf8615c90c5383ab771a577bc61
이석진 <seokjin.dev@gmail.com>
2019-05-31 23:49:34 +0900

    a lot of test codes...

--- Test/VectorTest.cpp
+++ Test/VectorTest.cpp
@@ -15,7 +15,7 @@ void AssertAllEqual(const ostl::vector<T>& actual, const T& expected) {
 		ASSERT_EQ(value, expected);
 }
 
-TEST(VectorTest, ConstructorTest) {
+TEST(Vector, Constructor) {
 	std::initializer_list<std::string> init{ "the", "frogurt", "is", "also", "cursed" };
 	ostl::vector<std::string> words1(init);
 	AssertAllEqual(words1, init);
@@ -32,7 +32,7 @@ TEST(VectorTest, ConstructorTest) {
 	AssertAllEqual<std::string>(words4, "Mo");
 }
 
-TEST(VectorTest, ReplaceTest) {
+TEST(Vector, Replace) {
 	const auto list = { 3, 1, 4, 6, 5, 9 };
 	const auto expected_size = list.size();
 
@@ -62,7 +62,42 @@ TEST(VectorTest, ReplaceTest) {
 	AssertAllEqual(nums3, list);
 }
 
-TEST(VectorTest, InsertTest) {
+TEST(Vector, Assign) {
+	ostl::vector<char> characters;
+	characters.assign(5, 'a');
+	AssertAllEqual(characters, 'a');
+}
+
+TEST(Vector, ElementAccess) {
+	ostl::vector<int> numbers{ 2,4,6,8 };
+	ASSERT_THROW(numbers.at(4), std::out_of_range);
+	ASSERT_EQ(numbers[1], 4);
+	numbers[0] = 5;
+	AssertAllEqual(numbers, { 5,4,6,8 });
+	ASSERT_EQ(numbers.front(), 5);
+	ASSERT_EQ(numbers.back(), 8);
+	ASSERT_EQ(numbers.data()[2], 6);
+}
+
+TEST(Vector, Iterator) {
+	ostl::vector<int> ints{ 1,2,4,8,16 };
+	ostl::vector<std::string> fruits{ "orange","apple","raspberry" };
+	ostl::vector<char> empty;
+
+	int sum = 0;
+	for (auto it = ints.cbegin(); it != ints.cend(); ++it)
+		sum += *it;
+	ASSERT_EQ(sum, 31);
+	ASSERT_EQ(*fruits.begin(), "orange");
+	ASSERT_EQ(empty.begin(), empty.end());
+
+	ASSERT_EQ(*ints.crbegin(), 16);
+	ASSERT_EQ(*(ints.crbegin() + 1), 8);
+	ASSERT_EQ(*(ints.crend() - 1), 1);
+	ASSERT_EQ(empty.rbegin(), empty.rend());
+}
+
+TEST(Vector, Insert) {
 	constexpr size_t size = 3;
 	constexpr int val = 100;
 	ostl::vector<int> vec(size, val);
